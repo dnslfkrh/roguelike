7e4c65185854112bf70b14358057d715e0fa29f6
yonguk <lyw3059@gmail.com>
2024-11-05 09:37:33 +0900

    feat: enemy follow player & reposition
    플레이어 따라오는 적, 무한맵처럼 멀어지면 이동

--- Great Wizard/Assets/Scripts/Reposition.cs
+++ Great Wizard/Assets/Scripts/Reposition.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    private void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -31,6 +38,13 @@ public class Reposition : MonoBehaviour
                 else if (diffX < diffY)
                 {
                     transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+
+            case "Enemy":
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f), 0));
                 }
                 break;
         }
